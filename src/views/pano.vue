e03f7a3c5436524c6edfc970804e700da272b1f9
xiDaiDai <xishiwei@sina.cn>
2019-05-26 16:20:37 +0800

    --

--- src/views/pano.vue
+++ src/views/pano.vue
@@ -31,11 +31,11 @@
     mounted() {
      let opt = {
         container: 'panoramaConianer',//容器
-        url: `/img/${this.roomName}.jpeg`,
+        url: `./img/${this.roomName}.jpeg`,
         lables: [
-          {position: {lon: -72.00, lat: 9.00}, logoUrl: '/img/logo.png', text: 'yy',code:'p1'},
-          {position: {lon: 114.12, lat: 69.48}, logoUrl: '/img/logo.png', text: '一片云彩',code:'p1'},
-          {position: {lon: 132.48, lat: -12.24}, logoUrl: '/img/logo.png', text: '大海',code:'p1'}
+          {position: {lon: -72.00, lat: 9.00}, logoUrl: './img/logo.png', text: 'yy',code:'p1'},
+          {position: {lon: 114.12, lat: 69.48}, logoUrl: './img/logo.png', text: '一片云彩',code:'p1'},
+          {position: {lon: 132.48, lat: -12.24}, logoUrl: './img/logo.png', text: '大海',code:'p1'}
         ],
         widthSegments: 60,//水平切段数
         heightSegments: 40,//垂直切段数（值小粗糙速度快，值大精细速度慢）
@@ -45,7 +45,7 @@
         sprite: 'icon', // label,icon
         onClick: this.onClick,
       }
-      new tpanorama(opt)
+      tp = new tpanorama(opt)
     },
 
     beforeDestroy(){
