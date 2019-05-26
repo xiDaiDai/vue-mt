1bdbde673046b2697676e9358b386773eccf6ed8
xiDaiDai <xishiwei@sina.cn>
2019-05-26 18:37:22 +0800

    --

--- src/views/phonepano.vue
+++ src/views/phonepano.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
-    <a-scene>
-      <a-sky src="https://xidaidai.github.io/vue-mt/build/img/room1.jpeg" rotation="0 -130 0" crossorigin="Anonymous"></a-sky>
-    </a-scene>
+    <!--<a-scene>-->
+      <!--<a-sky src="https://xidaidai.github.io/vue-mt/build/img/room1.jpeg" rotation="0 -130 0" crossorigin="Anonymous"></a-sky>-->
+    <!--</a-scene>-->
+    <div id="container"></div>
   </div>
 </template>
 
@@ -29,7 +30,34 @@
     },
 
     mounted() {
+      var div = document.getElementById('container');
+      let w =  window.innerWidth;
+      let h =  window.innerHeight;
+      var PSV = new PhotoSphereViewer({
+        // Path to the panorama
+        panorama: './img/room1.jpeg',
 
+        // Container
+        container: div,
+
+        // Deactivate the animation
+        time_anim: false,
+
+        // Display the navigation bar
+        navbar: true,
+
+        // Resize the panorama
+        size: {
+          width: w,
+          height: h
+        },
+
+        // 限制顶部
+        // tilt_up_max: Math.PI / 7,
+
+        // 限制底部
+        // tilt_down_max: Math.PI / 7
+      });
     },
 
     beforeDestroy(){
