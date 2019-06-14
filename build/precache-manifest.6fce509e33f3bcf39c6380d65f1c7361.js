ec199bb080a96f73102485695d1a9df407337d4c
xiDaiDai <xishiwei@sina.cn>
2019-06-14 15:46:09 +0800

    pwa

---
+++ build/precache-manifest.6fce509e33f3bcf39c6380d65f1c7361.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ec734c015aa0334be79b516cb704c69",
+    "url": "jslib/vr/DeviceOrientationControls.js"
+  },
+  {
+    "revision": "c83bbca03dbce1785e59",
+    "url": "static/css/app.css"
+  },
+  {
+    "revision": "011b6f8b4e46d7e16849",
+    "url": "static/css/chunk-0b35f2a5.css"
+  },
+  {
+    "revision": "011b6f8b4e46d7e16849",
+    "url": "static/js/chunk-0b35f2a5.js"
+  },
+  {
+    "revision": "8aaae22d847b00834d72",
+    "url": "static/css/chunk-13ff13ac.css"
+  },
+  {
+    "revision": "8aaae22d847b00834d72",
+    "url": "static/js/chunk-13ff13ac.js"
+  },
+  {
+    "revision": "0ab32a4e51a434276145",
+    "url": "static/css/chunk-277b1e94.css"
+  },
+  {
+    "revision": "0ab32a4e51a434276145",
+    "url": "static/js/chunk-277b1e94.js"
+  },
+  {
+    "revision": "14fb4b0803cd904c7c51",
+    "url": "static/js/chunk-vendors.js"
+  },
+  {
+    "revision": "0048667c5e8420a1ce8a",
+    "url": "static/css/orderlist.css"
+  },
+  {
+    "revision": "0048667c5e8420a1ce8a",
+    "url": "static/js/orderlist.js"
+  },
+  {
+    "revision": "c83bbca03dbce1785e59",
+    "url": "static/js/app.js"
+  },
+  {
+    "revision": "c99da25947f0a02fc9cbb0389e3ff7f2",
+    "url": "jslib/vr/browser.js"
+  },
+  {
+    "revision": "8ffbe2e0dba94b30c20aeeb1b4bd2c5f",
+    "url": "img/room2.jpeg"
+  },
+  {
+    "revision": "8e620ed7c3436affc4249703d908e33c",
+    "url": "jslib/vr/doT.js"
+  },
+  {
+    "revision": "fe3b8c857a7851e7ba4d75bc977c39b3",
+    "url": "jslib/vr/uevent.min.js"
+  },
+  {
+    "revision": "d70c01401f1689ba6b55487e7ecbe2af",
+    "url": "offline.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "cbcab8c512db22ae37b3010581e1e07c",
+    "url": "service-worker.js"
+  },
+  {
+    "revision": "8e4dd1896fed66584cdaad4b8971987d",
+    "url": "index.html"
+  },
+  {
+    "revision": "cf0fbffe6ec98381ce376023bb5e77dd",
+    "url": "jslib/vr/photo-sphere-viewer.css"
+  },
+  {
+    "revision": "3aa75382f7d5b0f08bbd26a738c347ec",
+    "url": "jslib/vr/photo-sphere-viewer.js"
+  },
+  {
+    "revision": "e9a09b1c1855661e7e798efcda1552bd",
+    "url": "img/room3.jpeg"
+  },
+  {
+    "revision": "217b7b35af76e115faf928097bdcffe5",
+    "url": "jslib/vr/three.js"
+  },
+  {
+    "revision": "6ddbdcc370cfba4c8ed4aa0c2b5a1458",
+    "url": "img/room1.jpeg"
+  },
+  {
+    "revision": "b99b358ed8e5db7d93d0b4edd716897a",
+    "url": "img/p1.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.png"
+  }
+];
